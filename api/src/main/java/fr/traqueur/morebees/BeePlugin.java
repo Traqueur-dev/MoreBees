c05d75e44dc1b5b9e2da6eead678888812af8aae
Traqueur_ <traqueur.dev@gmail.com>
2025-07-20 15:54:40 +0200

    feat: add messages and first config

--- api/src/main/java/fr/traqueur/morebees/BeePlugin.java
+++ api/src/main/java/fr/traqueur/morebees/BeePlugin.java
@@ -1,6 +1,10 @@
 package fr.traqueur.morebees;
 
+import fr.traqueur.morebees.settings.Settings;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class BeePlugin extends JavaPlugin {
+
+    public abstract Settings getSettings();
+
 }
